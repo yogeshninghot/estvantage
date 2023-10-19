d7c30cda3d40a73370a07d659751c61169f92855
Yogesh Ninghot <ydninghot@gmail.com>
2023-10-19 19:07:29 +0530

    Initial commit2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import React, { useEffect, useState } from "react";
+
+
 function App() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetchRandomUser();
+  }, []);
+
+  const fetchRandomUser = async () => {
+    try {
+      const response = await fetch("https://randomuser.me/api");
+      if (response.ok) {
+        const data = await response.json();
+        const user = data.results[0];
+        setUserData(user);
+        localStorage.setItem("randomUserData", JSON.stringify(user));
+      } else {
+        console.error("Error fetching user data:", response.statusText);
+      }
+    } catch (error) {
+      console.error("Error fetching user data:", error);
+    }
+  };
+
+  const refreshUser = () => {
+    fetchRandomUser();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='Heading'>Random User Information</h1>
+      {userData && userData.name && (
+        <div className="UserContainer">
+          <img
+            src={userData.picture.large}
+            alt={`${userData.name.first}'s picture`}
+            className="UserPicture"
+          />
+          <p className="FullName">
+            Full Name :-  {userData.name.first} {userData.name.last}
+          </p>
+          <p className="Email">Email Address :- {userData.email}</p>
+        </div>
+      )}
+      <button onClick={refreshUser} className='Refresh'>Refresh</button>
     </div>
   );
 }
